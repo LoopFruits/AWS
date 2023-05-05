3a8b355053244ee3f3e9580204bff955d95d0526
LoopFruits <Kensworldc@gmail.com>
2023-05-05 13:55:53 -0400

    fixed errors, working static site

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ class App extends React.Component {
               <p><strong>The Landon Hotel - London</strong> was recently renovated, and we considered the impact on the earth the entire way. From green building materials, to solar power, to energy-friendly lighting and appliances throughout the hotel - we’re saving energy in every socket, outlet, and switch. We’ve also initiated a recycling and composting program that reduces the load to local landfills, while providing valuable raw material for use in new products, or in the case of compost, for use in local gardens and landscapes.</p>
             </article>
           </div>
+        </main>
 
           <footer className="scene">
             <article className="content">
