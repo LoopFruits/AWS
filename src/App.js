0a0e93218af2d0703cbd561e5fa1b7100eb501d9
LoopFruits <Kensworldc@gmail.com>
2023-05-06 10:40:50 -0400

    cleaned code, imported footer to app js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Footer from './Components/Footer';
 
 
 class App extends React.Component { 
@@ -10,21 +11,8 @@ class App extends React.Component {
       <div className="App">
        <Header/>
        <Main/>
-
-        
-
-          <footer className="scene">
-            <article className="content">
-              <div id="socialmedia">
-                <ul className="group">
-                  <li><a href="https://twitter.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/twitter.png" alt="icon for twitter"/></a></li>
-                  <li><a href="http://www.facebook.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/facebook.png" alt="icon for facebook"/></a></li>
-                  <li><a href="http://www.youtube.com"><img className="icon" src="https://landonhotel.com/images/socialmedia/youtube.png" alt="icon for youtube"/></a></li>    
-                </ul>      
-              </div>
-            </article>
-          </footer>
-        </div>
+       <Footer/>
+      </div>
     );
   }
 }
